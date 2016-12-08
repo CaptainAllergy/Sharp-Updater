ee067f61b2439b347d435f5acf83e9bc401c157c
RAAK <tobias.raak@agilent.com>
2016-12-08 11:40:43 +0100

    Form Init

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpUpdate;
+
+namespace TestUpdater
+{
+    public partial class Form1 : Form, ISharpUpdateable
+    {
+        private SharpUpdater updater;
+        public Form1()
+        {
+            InitializeComponent();
+            this.label1.Text = this.ApplicationAssembly.GetName().Version.ToString();
+            updater = new SharpUpdater(this);
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly();
+            }
+        }
+
+        public Icon ApplicationIcon
+        {
+            get
+            {
+                return this.Icon;
+            }
+        }
+
+        public string ApplicationID
+        {
+            get
+            {
+                return "TestUpdater";
+            }
+        }
+
+        public string ApplicationName
+        {
+            get
+            {
+                return "TestUpdater";
+            }
+        }
+
+        public Form Context
+        {
+            get
+            {
+                return this;
+            }
+        }
+
+        public Uri UpdateXmlLocation
+        {
+            get
+            {
+                return new Uri("https://www.dropbox.com/home/TestUpdater/update.xml");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updater.DoUpdate();
+        }
+    }
+}
